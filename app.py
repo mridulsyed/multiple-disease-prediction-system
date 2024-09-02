49d4bbc1e536075ec2247b5575a4ac5480d70c57
SYED SIDDIQUE MRIDUL <mridulsyed11@gmail.com>
2024-09-02 22:02:07 +0600

    Update app.py

--- app.py
+++ app.py
@@ -26,6 +26,10 @@ diab_diagnosis = ''
 heart_diagnosis = ''
 parkinsons_diagnosis = ''
 
+# Helper function for hint HTML
+def hint_text(description):
+    return f'<div style="font-size: 12px; color: grey;">{description}</div>'
+
 # Diabetes Prediction Page
 if selected == 'Diabetes Prediction':
     st.title('Diabetes Prediction using ML')
@@ -33,21 +37,21 @@ if selected == 'Diabetes Prediction':
     # Input fields
     col1, col2, col3 = st.columns(3)
     with col1:
-        Pregnancies = st.text_input('Number of Pregnancies', '')
+        Pregnancies = st.text_input('Number of Pregnancies', placeholder='e.g. 2', help=hint_text('Normal range: 0-20'))
     with col2:
-        Glucose = st.text_input('Glucose Level', '')
+        Glucose = st.text_input('Glucose Level', placeholder='e.g. 120', help=hint_text('Normal range: 70-180'))
     with col3:
-        BloodPressure = st.text_input('Blood Pressure value', '')
+        BloodPressure = st.text_input('Blood Pressure value', placeholder='e.g. 80', help=hint_text('Normal range: 60-90'))
     with col1:
-        SkinThickness = st.text_input('Skin Thickness value', '')
+        SkinThickness = st.text_input('Skin Thickness value', placeholder='e.g. 20', help=hint_text('Normal range: 0-99'))
     with col2:
-        Insulin = st.text_input('Insulin Level', '')
+        Insulin = st.text_input('Insulin Level', placeholder='e.g. 100', help=hint_text('Normal range: 0-1000'))
     with col3:
-        BMI = st.text_input('BMI value', '')
+        BMI = st.text_input('BMI value', placeholder='e.g. 25', help=hint_text('Normal range: 18-30'))
     with col1:
-        DiabetesPedigreeFunction = st.text_input('Diabetes Pedigree Function value', '')
+        DiabetesPedigreeFunction = st.text_input('Diabetes Pedigree Function value', placeholder='e.g. 0.5', help=hint_text('Normal range: 0-2.5'))
     with col2:
-        Age = st.text_input('Age of the Person', '')
+        Age = st.text_input('Age of the Person', placeholder='e.g. 30', help=hint_text('Normal range: 0-100'))
 
     # Prediction logic
     if st.button('Diabetes Test Result'):
@@ -79,31 +83,31 @@ if selected == 'Heart Disease Prediction':
     # Input fields
     col1, col2, col3 = st.columns(3)
     with col1:
-        age = st.text_input('Age', '')
+        age = st.text_input('Age', placeholder='e.g. 55', help=hint_text('Normal range: 20-100'))
     with col2:
-        sex = st.text_input('Sex', '')
+        sex = st.text_input('Sex', placeholder='e.g. 1 for male', help=hint_text('0 = female, 1 = male'))
     with col3:
-        cp = st.text_input('Chest Pain types', '')
+        cp = st.text_input('Chest Pain types', placeholder='e.g. 1', help=hint_text('Normal range: 0-4'))
     with col1:
-        trestbps = st.text_input('Resting Blood Pressure', '')
+        trestbps = st.text_input('Resting Blood Pressure', placeholder='e.g. 130', help=hint_text('Normal range: 90-200'))
     with col2:
-        chol = st.text_input('Serum Cholestoral in mg/dl', '')
+        chol = st.text_input('Serum Cholestoral in mg/dl', placeholder='e.g. 250', help=hint_text('Normal range: 150-500'))
     with col3:
-        fbs = st.text_input('Fasting Blood Sugar > 120 mg/dl', '')
+        fbs = st.text_input('Fasting Blood Sugar > 120 mg/dl', placeholder='e.g. 1', help=hint_text('0 = false, 1 = true'))
     with col1:
-        restecg = st.text_input('Resting Electrocardiographic results', '')
+        restecg = st.text_input('Resting Electrocardiographic results', placeholder='e.g. 0', help=hint_text('Normal range: 0-2'))
     with col2:
-        thalach = st.text_input('Maximum Heart Rate achieved', '')
+        thalach = st.text_input('Maximum Heart Rate achieved', placeholder='e.g. 150', help=hint_text('Normal range: 60-220'))
     with col3:
-        exang = st.text_input('Exercise Induced Angina', '')
+        exang = st.text_input('Exercise Induced Angina', placeholder='e.g. 1', help=hint_text('0 = no, 1 = yes'))
     with col1:
-        oldpeak = st.text_input('ST depression induced by exercise', '')
+        oldpeak = st.text_input('ST depression induced by exercise', placeholder='e.g. 1.2', help=hint_text('Normal range: 0-6'))
     with col2:
-        slope = st.text_input('Slope of the peak exercise ST segment', '')
+        slope = st.text_input('Slope of the peak exercise ST segment', placeholder='e.g. 2', help=hint_text('Normal range: 0-2'))
     with col3:
-        ca = st.text_input('Major vessels colored by flourosopy', '')
+        ca = st.text_input('Major vessels colored by fluoroscopy', placeholder='e.g. 0', help=hint_text('Normal range: 0-3'))
     with col1:
-        thal = st.text_input('thal: 0 = normal; 1 = fixed defect; 2 = reversible defect', '')
+        thal = st.text_input('thal: 0 = normal; 1 = fixed defect; 2 = reversible defect', placeholder='e.g. 2', help=hint_text('Normal range: 0-2'))
 
     # Prediction logic
     if st.button('Heart Disease Test Result'):
@@ -140,83 +144,16 @@ if selected == "Parkinsons Prediction":
     # Input fields
     col1, col2, col3, col4, col5 = st.columns(5)
     with col1:
-        fo = st.text_input('MDVP:Fo(Hz)', '')
+        fo = st.text_input('MDVP:Fo(Hz)', placeholder='e.g. 150', help=hint_text('Normal range: 70-400'))
     with col2:
-        fhi = st.text_input('MDVP:Fhi(Hz)', '')
+        fhi = st.text_input('MDVP:Fhi(Hz)', placeholder='e.g. 200', help=hint_text('Normal range: 100-500'))
     with col3:
-        flo = st.text_input('MDVP:Flo(Hz)', '')
+        flo = st.text_input('MDVP:Flo(Hz)', placeholder='e.g. 100', help=hint_text('Normal range: 50-300'))
     with col4:
-        Jitter_percent = st.text_input('MDVP:Jitter(%)', '')
+        Jitter_percent = st.text_input('MDVP:Jitter(%)', placeholder='e.g. 0.5', help=hint_text('Normal range: 0-1'))
     with col5:
-        Jitter_Abs = st.text_input('MDVP:Jitter(Abs)', '')
+        Jitter_Abs = st.text_input('MDVP:Jitter(Abs)', placeholder='e.g. 0.001', help=hint_text('Normal range: 0-0.01'))
     with col1:
-        RAP = st.text_input('MDVP:RAP', '')
+        RAP = st.text_input('MDVP:RAP', placeholder='e.g. 0.2', help=hint_text('Normal range: 0-0.5'))
     with col2:
-        PPQ = st.text_input('MDVP:PPQ', '')
-    with col3:
-        DDP = st.text_input('Jitter:DDP', '')
-    with col4:
-        Shimmer = st.text_input('MDVP:Shimmer', '')
-    with col5:
-        Shimmer_dB = st.text_input('MDVP:Shimmer(dB)', '')
-    with col1:
-        APQ3 = st.text_input('Shimmer:APQ3', '')
-    with col2:
-        APQ5 = st.text_input('Shimmer:APQ5', '')
-    with col3:
-        APQ = st.text_input('MDVP:APQ', '')
-    with col4:
-        DDA = st.text_input('Shimmer:DDA', '')
-    with col5:
-        NHR = st.text_input('NHR', '')
-    with col1:
-        HNR = st.text_input('HNR', '')
-    with col2:
-        RPDE = st.text_input('RPDE', '')
-    with col3:
-        DFA = st.text_input('DFA', '')
-    with col4:
-        spread1 = st.text_input('spread1', '')
-    with col5:
-        spread2 = st.text_input('spread2', '')
-    with col1:
-        D2 = st.text_input('D2', '')
-    with col2:
-        PPE = st.text_input('PPE', '')
-
-    # Prediction logic
-    if st.button("Parkinson's Test Result"):
-        try:
-            features = [
-                float(fo),
-                float(fhi),
-                float(flo),
-                float(Jitter_percent),
-                float(Jitter_Abs),
-                float(RAP),
-                float(PPQ),
-                float(DDP),
-                float(Shimmer),
-                float(Shimmer_dB),
-                float(APQ3),
-                float(APQ5),
-                float(APQ),
-                float(DDA),
-                float(NHR),
-                float(HNR),
-                float(RPDE),
-                float(DFA),
-                float(spread1),
-                float(spread2),
-                float(D2),
-                float(PPE)
-            ]
-            parkinsons_prediction = parkinsons_model.predict([features])
-            if parkinsons_prediction[0] == 1:
-                parkinsons_diagnosis = "The person has Parkinson's disease"
-            else:
-                parkinsons_diagnosis = "The person does not have Parkinson's disease"
-        except ValueError:
-            parkinsons_diagnosis = 'Please enter valid input values.'
-
-    st.success(parkinsons_diagnosis)
+        PPQ = st.text_input('MDVP:PPQ', placeholder='e.g. 0.3', help=hint_text('Normal range: 0-0.5'))
